19b5a84d1dcc7d44d7b1730daa99dc4bcc6ae294
Levente26 <madi.levente26@gmail.com>
2022-04-15 00:46:58 +0200

    transitions & animations

--- shared.js
+++ shared.js
@@ -4,12 +4,15 @@ let modal = document.querySelector('.modal')
 let modalNoButton = document.getElementById('no')
 let toggleButton = document.querySelector('.toggle-button')
 let mobileNav = document.querySelector('.mobile-nav')
-
+let ctaButton = document.querySelector('.main-nav__item--cta')
 
 for (let i = 0; i < selectPlanButton.length; i++) {
     selectPlanButton[i].addEventListener('click', () => {
         modal.classList.add('open')
         backdrop.classList.add('open')
+        setTimeout(() => {
+            backdrop.style.display = "block"
+        }, 10)
     })
 }
 
@@ -17,6 +20,9 @@ if(modalNoButton) {
     modalNoButton.addEventListener("click", () => {
         modal.classList.remove('open')
         backdrop.classList.remove('open')
+        setTimeout(() => {
+            backdrop.style.display = "none"
+        }, 200)
     })
 }
 
@@ -26,13 +32,30 @@ backdrop.addEventListener("click", () => {
     }
     backdrop.classList.remove('open')
     mobileNav.classList.remove('open')
+    setTimeout(() => {
+        backdrop.style.display = "none"
+    }, 200)
 })
 
 toggleButton.addEventListener("click", () => {
     mobileNav.classList.add('open')
     backdrop.classList.add('open')
+    setTimeout(() => {
+        backdrop.style.display = "block"
+    }, 200)
 })
 
+ctaButton.addEventListener('animationstart', (e) => {
+    console.log("Animation started", e)
+})
+
+ctaButton.addEventListener('animationend', (e) => {
+    console.log("Animation ended", e)
+})
+
+ctaButton.addEventListener('animationiteration', (e) => {
+    console.log("Animation iteration", e)
+})
 
 
 
